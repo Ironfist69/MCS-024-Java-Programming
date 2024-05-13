6bac7a3ed94ded89c61101b6b2fca05dd0c853b2
Ironfist69 <subhabratasen1999@gmail.com>
2024-05-13 18:27:37 +0530

    Updated

---
+++ June 2022/appendStringToStringBuffer.java
@@ -0,0 +1,7 @@
+public  class appendStringToStringBuffer {
+    public static void main (String[] args) {
+        StringBuffer sb=new StringBuffer("Java is a ");
+        sb.append("useless Program");
+        System.out.println(sb);
+    }
+}
